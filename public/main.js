6fba280e3d5d51bd163e81271e6e89c7122d68e1
GMorelle-2412 <geoffreymorelle2412@gmail.com>
2026-01-14 15:19:13 +0100

    commit

--- public/main.js
+++ public/main.js
@@ -1,5 +1,6 @@
 const monInput = document.getElementById('monInput');
 const monBouton = document.getElementById('monBouton');
+const monBouton_2 = document.getElementById("monBouton_2")
 
 monBouton.addEventListener('click', () => {
     fetch('/register', {
@@ -7,9 +8,18 @@ monBouton.addEventListener('click', () => {
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ inputValue: monInput.value })     
+        body: JSON.stringify({ inputValue: monInput.value })
     }).then(response => response.text())
-      .then(data => {
-          alert(data);
-      });
+        .then(data => {
+            alert(data);
+        });
 });
+monBouton_2.addEventListener('click', () => {
+    fetch('info').then(
+        response => response.json()
+    ).then(
+        JsonResponse => {
+            document.getElementById('reponse').innerHTML = JsonResponse.cle1;
+        }
+    );
+})
