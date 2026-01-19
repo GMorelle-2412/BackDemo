8bcc3e060992ba63ef2605492a840f5a56acd305
GMorelle-2412 <geoffreymorelle2412@gmail.com>
2026-01-19 11:59:22 +0100

    commit

--- serveur.js
+++ serveur.js
@@ -30,7 +30,7 @@ app.get('/login', (req, res) => {
 
 
 app.get('/info', (req, res) => {
-  res.json({ cle1: 'valeur1', cle2: 'valeur2' });
+  res.json({ cle1: 'valeur1', cle2: 'valeur2'});
 });
 
 
