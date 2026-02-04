ff4e8d6b8bf7c135a89c642b96ff2eb0215d33d8
GMorelle-2412 <geoffreymorelle2412@gmail.com>
2026-02-04 12:00:30 +0100

    affinement

--- public/main.js
+++ public/main.js
@@ -5,7 +5,7 @@ const connection = document.getElementById("monBouton_2");
 const monBouton_3 = document.getElementById("monBouton_3");
 const monBoutonvote = document.getElementById("Vote");
 
-var i = 1;
+var i = 0;
 
 monBouton.addEventListener('click', () => {
     fetch('/register', {
@@ -13,7 +13,7 @@ monBouton.addEventListener('click', () => {
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ loginValue: login.value ,passwordValue: Password.value})
+        body: JSON.stringify({ loginValue: login.value, passwordValue: Password.value })
     }).then(response => response.text())
         .then(data => {
             alert(data);
@@ -23,8 +23,8 @@ monBouton.addEventListener('click', () => {
 
 monBoutonvote.addEventListener('click', () => {
     const usersList = document.getElementById('usersList');
-    const selectedUserId = usersList.value;   
-    alert('Utilisateur : ' + selectedUserId+ ' à voter');
+    const selectedUserId = usersList.value;
+    alert('Utilisateur : ' + selectedUserId + ' à voter');
 
 
     //récupérer aussi id du connecté dans le local storage
@@ -34,12 +34,12 @@ monBoutonvote.addEventListener('click', () => {
 
 
     fetch('/votes', {
-        
+
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ idValue: selectedUserId})
+        body: JSON.stringify({ idValue: selectedUserId })
     }).then(response => response.text())
         .then(data => {
             alert(data);
@@ -48,24 +48,24 @@ monBoutonvote.addEventListener('click', () => {
 
 window.onload = () => {
     fetch('/users')
-    .then(response => response.json())
-    .then(users => {
-        const usersList = document.getElementById('usersList');
-        users.forEach(user => {
-            //création d'un input select option avec id en value et login en texte  
-            const option = document.createElement('option');
-            option.value = user.id;
-            option.text = user.Login;
-            usersList.appendChild(option);  
+        .then(response => response.json())
+        .then(users => {
+            const usersList = document.getElementById('usersList');
+            users.forEach(user => {
+                //création d'un input select option avec id en value et login en texte  
+                const option = document.createElement('option');
+                option.value = user.id;
+                option.text = user.Login;
+                usersList.appendChild(option);
+            });
         });
-    });
 }
 
 const userSelectedButton = document.getElementById('userSelectedButton');
 
 userSelectedButton.addEventListener('click', () => {
     const usersList = document.getElementById('usersList');
-    const selectedUserId = usersList.value;     
+    const selectedUserId = usersList.value;
     alert('Utilisateur sélectionné ID : ' + selectedUserId);
 });
 
@@ -78,13 +78,20 @@ connection.addEventListener('click', () => {
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ login: login, password: password})
+        body: JSON.stringify({ login: login, password: password })
     }).then(response => response.json())
         .then(data => {
             alert(data.message);
             alert('ID utilisateur : ' + data.user.id);
             localStorage.setItem(login, data.user.id);
         });
+
+        fetch('/info')
+            .then(responsebrute => responsebrute.json())
+            .then(
+                responsejson => {
+                    document.getElementById('reponse').innerHTML = responsejson.cle2;
+                });
 });
 
 /*
